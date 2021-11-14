e8b423770401de8edbbaf40e704bceefb78f9beb
WilliamTodd93 <70246553+WilliamTodd93@users.noreply.github.com>
2021-11-14 11:20:45 +1000

    Removed example game

--- games/example-game/input.js
+++
@@ -1,9 +0,0 @@
-class Input {
-
-    static GetAxis() {
-        let x = keyIsDown(LEFT_ARROW) ? -1 : keyIsDown(RIGHT_ARROW) ? 1 : 0
-        let y = keyIsDown(UP_ARROW) ? -1 : keyIsDown(DOWN_ARROW) ? 1 : 0
-        console.log(`${x} ${y}`)
-        return createVector(x, y)
-    }
-}
